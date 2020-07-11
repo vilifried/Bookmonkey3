805dfe6045ad548cb782256bc94dab4fb5ca5566
Wilfried Haderer <w.haderer@gmx.net>
2020-07-11 15:12:35 +0200

    @input @output

---
+++ src/app/book-list-item/book-list-item.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Book} from "../shared/book";
+
+@Component({
+    selector: 'bm-book-list-item',
+    templateUrl: './book-list-item.component.html',
+    styleUrls: ['./book-list-item.component.css']
+})
+export class BookListItemComponent implements OnInit {
+
+    @Input() book: Book;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
